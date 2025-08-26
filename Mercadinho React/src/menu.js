62eb186c047a5854547cc66d744485960e1ca435
VicenzoFr <vicenzofr@gmail.com>
2025-08-25 23:26:47 -0300

    animacao do menu funcionando

--- Mercadinho React/src/menu.js
+++ Mercadinho React/src/menu.js
@@ -1,6 +1,6 @@
 function Menu({onUserClick}){
     return(
-        <div className="w-17 h-110 bg-[#F9FAFB] absolute right-0 grid gap-2 justify-center items-center ">
+        <div className="w-17 h-110 bg-[#F9FAFB] absolute right-0 grid gap-2 justify-center items-center animate__animated animate__slideInRight">
             <button className="w-7 h-10 bg-[url('./assets/icons/menu/user.png')] bg-no-repeat bg-center bg-contain cursor-pointer" onClick={onUserClick}></button>
             <button className="w-7 h-10 bg-[url('./assets/icons/menu/grocery-store.png')] bg-no-repeat bg-center bg-contain cursor-pointer"></button>
             <button className="w-7 h-10 bg-[url('./assets/icons/menu/plus.png')] bg-no-repeat bg-center bg-contain cursor-pointer"></button>
