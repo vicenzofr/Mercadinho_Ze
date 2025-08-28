a5fadea3143aa822c0eb269891e12393a832e700
VicenzoFr <vicenzofr@gmail.com>
2025-08-28 15:08:50 -0300

    folder api add and change of node_modules

---
+++ Mercadinho React/api/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) =>{
+    res.send("api mercadinho")
+})
+
+
+app.listen(3000, () => {
+    console.log('Server in running on port 3000');
+})
