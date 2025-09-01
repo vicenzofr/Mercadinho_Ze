1239fa21199f3ec91306ca162c2d1146176e57b6
VicenzoFr <vicenzofr@gmail.com>
2025-09-01 15:16:04 -0300

    Footer ok

--- Mercadinho_Ze/app.js
+++ Mercadinho_Ze/app.js
@@ -54,6 +54,8 @@ function App() {
           </div>
       </>
       )}
+        
+        <window.Footer/>
     </main>    
   );
 }
--- Mercadinho_Ze/src/footer.js
+++ Mercadinho_Ze/src/footer.js
@@ -1,10 +1,10 @@
-// function Footer() {
-//   return (
-//     <footer className="w-full bg-[#0F3A33] text-center  mt-20">
-//       <p className="text-white">
-//         &copy; 2025 VicenzoFr. Todos os direitos reservados.
-//       </p>
-//     </footer>
-//   );
-// }
-// window.Footer = Footer;
+ function Footer() {
+   return (
+     <footer className="w-full h-15 bg-[#0F3A33] text-center items-center justify-center flex mt-51">
+       <p className="text-white">
+         &copy; 2025 VicenzoFr. Todos os direitos reservados.
+       </p>
+     </footer>
+   );
+ }
+ window.Footer = Footer;
