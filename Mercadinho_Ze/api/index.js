b777b5fcc4898b99f81eca90bd0c54af076f7caa
VicenzoFr <vicenzofr@gmail.com>
2025-09-01 22:57:40 -0300

    DataBase with SqLite

--- Mercadinho_Ze/api/index.js
+++ Mercadinho_Ze/api/index.js
@@ -1,13 +1,21 @@
-const express = require('express');
+const express = require("express");
+const sqlite3 = require("sqlite3").verbose();
 const app = express();
-
-app.use(express.json());
-
-app.get('/', (req, res) =>{
-    res.send("api mercadinho")
-})
+const port = 3000;
 
 
-app.listen(3000, () => {
-    console.log('Server in running on http://localhost:3000');
-})
+const db = new sqlite3.Database("Mercadinho_Ze/api/database/mercadinho.db", (err) => {
+  if (err) {
+    console.error("Erro ao conectar no banco:", err.message);
+  } else {
+    console.log("Conectado ao banco SQLite!");
+  }
+});
+
+app.get("/", (req, res) => {
+  res.send("API rodando com SQLite!");
+});
+
+app.listen(port, () => {
+  console.log(`http://localhost:3000`);
+});
